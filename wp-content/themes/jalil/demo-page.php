9b620676ac0a00cfc99b0013e5db53f10b4bd02e
Jalil <abdullacelil00@gmail.com>
2024-10-12 16:24:38 +0400

    Create a PHP file

---
+++ wp-content/themes/jalil/demo-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *  Template Name: Demo Page Template
+ *  The template for displaying demo pages
+ */
+get_header();
+/* if(have_posts()) : while(have_posts()) : the_post();
+endwhile: endif; */
+if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+    }
+} else {
+    // what happens if there are no posts
+}
+
+get_sidebar();
+get_footer();
